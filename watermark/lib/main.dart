e073b1c53c07db39023aa6b4886de3f382e90563
jiangjun <1440779243@qq.com>
2024-02-26 08:41:14 +0800

    水印demo

--- watermark/lib/main.dart
+++ watermark/lib/main.dart
@@ -1,4 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +17,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +39,160 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? _imagePath;
+  ui.Image? _image;
+  ui.Image? _composeImg;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    createImage();
+  }
+
+  void createImage() {
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) async {
+      _image = await stringToImage(
+        text: '自定义🦟🧚🏼',
+        textStyle: ui.TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 48.0,
+          color: Colors.red,
+        ),
+      );
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            _imagePath != null
+                ? Image.asset(_imagePath!)
+                : const Text('no select img'),
+            _image != null
+                ? RawImage(image: _image, fit: BoxFit.cover)
+                : const Text('no create img'),
+            _composeImg != null
+                ? RawImage(image: _composeImg!)
+                : const Text('no compose img'),
+            MaterialButton(
+              onPressed: openGallery,
+              child: const Text('选择图片'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            MaterialButton(
+              onPressed: composeImage,
+              child: const Text('合成图片'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void openGallery() async {
+    PermissionStatus permissionStatus;
+    bool allow = true;
+    if (Platform.isAndroid) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      if (androidInfo.version.sdkInt >= 33) {
+        permissionStatus = await Permission.photos.status;
+        if (permissionStatus != PermissionStatus.granted) {
+          allow = false;
+        } else {
+          allow = true;
+        }
+      } else {
+        permissionStatus = await Permission.storage.status;
+        if (permissionStatus != PermissionStatus.granted) {
+          allow = false;
+        } else {
+          allow = true;
+        }
+      }
+    } else {
+      permissionStatus = await Permission.photos.request();
+      if (permissionStatus != PermissionStatus.granted) {
+        allow = false;
+      } else {
+        allow = true;
+      }
+    }
+    if (allow) {
+      XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (file != null) {
+        _imagePath = file.path;
+        setState(() {});
+      }
+    } else {
+      // showOpenSettingDialog(tip: '您未开启相册权限，请先授予相册权限');
+      debugPrint('您未开启相册权限，请先授予相册权限');
+    }
+  }
+
+  Future<ui.Image> stringToImage({
+    required String text,
+    required ui.TextStyle textStyle,
+  }) async {
+    Size size = MediaQuery.of(context).size;
+    final picRecorder = ui.PictureRecorder();
+    final paragraphBuilder = ui.ParagraphBuilder(ui.ParagraphStyle());
+    paragraphBuilder.pushStyle(textStyle);
+    paragraphBuilder.addText(text);
+    final paragraph = paragraphBuilder.build()
+      ..layout(ui.ParagraphConstraints(width: size.width));
+    final lineMetrics = paragraph.computeLineMetrics();
+    var width = 0.0;
+    for (var element in lineMetrics) {
+      if (element.width > width) {
+        width = element.width;
+      }
+    }
+    final cvs =
+        Canvas(picRecorder, Rect.fromLTRB(0, 0, width, paragraph.height));
+    cvs.drawParagraph(paragraph, const Offset(0.0, 0.0));
+    final pic = picRecorder.endRecording();
+    final img = await pic.toImage(width.toInt(), paragraph.height.toInt());
+    return img;
+  }
+
+  void composeImage() async {
+    final originByteData = await rootBundle.load(_imagePath!);
+    final srcByteData =
+        await _image!.toByteData(format: ui.ImageByteFormat.png);
+    final originImg = img.decodeImage(originByteData.buffer.asUint8List());
+    final srcImg = img.decodeImage(srcByteData!.buffer.asUint8List());
+    final newImg = img.compositeImage(
+      originImg!,
+      srcImg!,
+      center: true,
+    );
+    final cmd = img.Command();
+    cmd.image(newImg);
+    cmd.encodePng();
+    debugPrint('合成图片正在编码并转换为uint8List ...');
+    final uint8List = await cmd.getBytesThread();
+    debugPrint('编码转换完成');
+    _composeImg = await _fun2(uint8List!);
+    /// 下面这种方法会阻塞UI线程导致卡顿
+    // final uint8List = img.encodePng(newImg);
+    // _composeImg = await _fun2(img.encodePng(newImg));
+    setState(() {});
+  }
+
+
+  Future<ui.Image?> _fun2(Uint8List list) async {
+    Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(list, (ui.Image callBack) {
+      completer.complete(callBack);
+    });
+    return completer.future;
   }
 }
